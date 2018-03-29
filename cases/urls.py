7dda8ceb2222bc52b7af529184fc0095c6d10601
Kevin Svetlitski <kevin_svetlitski@berkeley.edu>
2018-03-29 05:04:31 -0500

    Preparing for merge.

--- cases/urls.py
+++ cases/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'cases'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
-    path('caseworkers', views.IndexView.as_view(), name='index'),
+    path('caseworkers/', views.IndexView.as_view(), name='index'),
     path('intake', views.IntakeView.as_view(), name='intake'),
     path('<str:slug>', views.PersonDetailView.as_view(), name='detail'),
 ]
