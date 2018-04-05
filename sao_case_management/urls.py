9267e6ce8533d149ebfaf96059a7745a9f84242a
Kevin Svetlitski <kevin_svetlitski@berkeley.edu>
2018-04-04 19:03:08 -0700

    Renamed project.

--- sao_case_management/urls.py
+++ sao_case_management/urls.py
@@ -1,4 +1,4 @@
-"""case_management_demo URL Configuration
+"""sao-case-management URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
--- sao_case_management/wsgi.py
+++ sao_case_management/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for case_management_demo project.
+WSGI config for sao-case-management project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "case_management_demo.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sao-case-management.settings")
 
 application = get_wsgi_application()
