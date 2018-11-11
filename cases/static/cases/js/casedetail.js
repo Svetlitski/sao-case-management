0338bd29509c2f791579c296439fa6bfbb0adb71
Svetlitski <kevin_svetlitski@berkeley.edu>
2018-11-10 19:13:55 -0800

    Fixes for mobile.

--- cases/static/cases/js/casedetail.js
+++ cases/static/cases/js/casedetail.js
@@ -1,8 +1,9 @@
 $(function() {
         $("#updates").accordion({ heightStyle: "content", collapsible: true });
         $("#case-description").accordion({ heightStyle: "content", collapsible: true });
-        $clientInformation = $('#info-block');
-        originalWidth = $clientInformation.outerWidth(); //deliberately global
+        $infoBlock = $('#info-block');
+        originalWidth = $infoBlock.outerWidth();
+        
 
 
     });
@@ -13,20 +14,18 @@ $(function() {
             if (!$(this).data('clicked')) {
                 $(this).data('clicked', true);
                 $(this).children().first().attr('class', 'fa fa-angle-right');
-                // $clientInformation = $('#client-information')
                 $('#update-client-info-btn').fadeOut(175);
-                $('#client-information').fadeOut(500);
-                $clientInformation.css({'overflow':'hidden', 'white-space': 'nowrap'})
-                $clientInformation.animate({width: 20})
+                $('#client-information').fadeOut(350);
+                $infoBlock.css({'overflow':'hidden', 'white-space': 'nowrap'})
+                $('')
+                $infoBlock.animate({width: $('#show-client-information-toggle').outerWidth()})
                 $('title').text('Case details');
             } else {
                 $(this).data('clicked', false);
                 $(this).children().first().attr('class', 'fa fa-angle-left');
                 $('#client-information').fadeIn(500);
                 $('#update-client-info-btn').fadeIn(200);
-                
-                $clientInformation.animate({width: 480});//$clientInformation.css({'overflow': 'visible', 'white-space': 'normal','width': 'auto'}));
-                // $clientInformation.css({'overflow': 'visible', 'white-space': 'normal', 'width': 'auto'});
+                $infoBlock.animate({width: originalWidth});
                 $('title').text(clientName); // clientName is global var declared in template
             }
         });
